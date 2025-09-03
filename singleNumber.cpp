60f92aebd32a3a3ab350f59863a47ba35bfee198
DBJ-git <divyjoshi.9999@gmail.com>
2025-09-03 23:04:40 +0530

    Create singleNumber.cpp

---
+++ singleNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int singleNumber(vector<int>& num){
+    int ans = 0;
+    for(int val : num){
+       ans = ans ^ val;
+    }
+    return ans;
+}
+int main() {
+    vector<int>num = {4,1,2,1,2};
+    
+    singleNumber(num)
+    return 0;
+}
