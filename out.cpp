d238adf205c77433374517de6070c9b5612bc816
DBJ-git <divyjoshi.9999@gmail.com>
2025-09-03 22:36:24 +0530

    Create out.cpp

---
+++ out.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int marks[10] = {100,99,98,97,96,95,94,93,92,91};
+    int size = 10;
+
+    for(int i = 0 ; i < size ; i++){
+        cout << marks[i] << endl;
+    }
+
+    return 0;
+}
